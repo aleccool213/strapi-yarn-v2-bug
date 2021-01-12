ca350711e37f0e35b7df283beb3abd0bddd4f984
Alec Brunelle <alec@alec.coffee>
2021-01-12 13:31:26 -0800

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a1c41c7d96b1727ed986181180f942f'),
+    },
+  },
+});
